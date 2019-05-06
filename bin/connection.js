d7c7f1aa0a4f16b1205aaf5d196b621bc84efad1
Marcelo B H <marceloborellihegedus@gmail.com>
2019-05-06 07:57:51 -0300

    adding new mongodb atlas uri

--- bin/connection.js
+++ bin/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 //mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/mypetpass`, {useNewUrlParser: true})
-mongoose.connect(`mongodb+srv://mypetpass:segredodopetpass@cluster0-hygow.mongodb.net/test`, {useNewUrlParser: true})
+//mongoose.connect(`mongodb+srv://mypetpass:segredodopetpass@cluster0-hygow.mongodb.net/test`, {useNewUrlParser: true})
+mongoose.connect(`mongodb+srv://mynewpetpass:!1CarlsagaN1!@cluster0-lv8no.mongodb.net/test?retryWrites=true`)
 
 module.exports = mongoose
